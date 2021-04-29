ac15cf463cfe1f423a70cbbb6b3e1e96d50c6d13
pratik kumar <kumarpratik861@gmail.com>
2021-04-29 23:39:54 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import { useState } from "react";
+import { useState, Profiler } from "react";
 import "./styles.css";
 
 export default function App() {
   const api = `https://randomuser.me/api`;
-
-  const [user, setUser] = useState("nothing");
-
+  const [user, setUser] = useState([]);
   const addUserHandler = async () => {
     const userData = await fetch(api, {
       method: "GET"
     });
     const userJson = await userData.json();
-    console.log(userJson);
-    setUser(userJson.results[0].gender);
+    const newUser = [...user, userJson.results[0]];
+    setUser(newUser);
   };
-
   return (
-    <div>
-      <button onClick={addUserHandler}>Add User</button>
-      <div>{user}</div>
+    <div className="app">
+      <Button addUserHandler={addUserHandler} />
+      <Profiler
+        id="userlist"
+        onRender={(id, phase, actualDuration) => {
+          console.log({
+            id,
+            phase,
+            actualDuration
+          });
+        }}
+      >
+        <UserList user={user} />
+      </Profiler>
     </div>
   );
 }
+
+const Button = ({ addUserHandler }) => {
+  return <button onClick={addUserHandler}>Add User</button>;
+};
+
+const UserList = (props) => {
+  const { user } = props;
+  return (
+    <>
+      <div className="user-list">
+        {user.map((userObj, idx) => {
+          return <UserObject key={idx} userObj={userObj} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+const UserObject = ({ userObj }) => {
+  return (
+    <div className="user-object">
+      {`${
+        userObj.name.title + " " + userObj.name.first + " " + userObj.name.last
+      }`}
+      <ol>
+        <li>{userObj.gender.toUpperCase()}</li>
+        <li>{userObj.email}</li>
+      </ol>
+    </div>
+  );
+};
